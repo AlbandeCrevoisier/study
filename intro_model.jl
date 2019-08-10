e4468e14f4263b4cc32ac54e601f00da8b164ce5
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-08-10 21:54:04 +0200

    Render sine function.

--- intro_model.jl
+++ intro_model.jl
@@ -1,4 +1,5 @@
 using Gen
+using PyPlot
 
 @gen function doubleaddr()
 	@trace(normal(0, 1), :x)
@@ -13,3 +14,25 @@ end;
 		@trace(normal(amplitude * sin(x * period / (2 * pi) + phase), 0.1), (:y, i))
 	end
 end;
+
+@gen function render_sine(trace)
+	xs = get_args(trace)[1]
+	ys = [trace[(:y, i)] for i=1:length(xs)]
+	xmin = minimum(xs)
+	xmax = maximum(xs)
+	phase = trace[:phase]
+	period = trace[:period]
+	amplitude = trace[:amplitude]
+	x = range(xmin, xmax, step=0.1)
+	y = amplitude * sin.(x * period / (2 * pi) .+ phase)
+	scatter(xs, ys)
+	ax = gca()
+	ax.set_xlim((xmin, xmax))
+	ax.set_ylim((- amplitude, amplitude))
+	plot(x, y, alpha=0.5)
+	plt.show()
+end;
+
+figure(figsize=(3, 3))
+t = Gen.simulate(sine, (range(0, stop=20),))
+render_sine(t);
