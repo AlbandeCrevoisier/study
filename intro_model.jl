ed3d113844f3f2cd2f6af381e02fa0c735a94b21
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-08-20 20:24:05 +0200

    Improve exercise naming scheme.

--- intro_model.jl
+++ intro_model.jl
@@ -2,13 +2,13 @@ ENV["MPLBACKEND"]="TkAgg"
 
 using Gen, PyPlot
 
-# 2 - Exercise
+# Part 2 - Exercise
 @gen function doubleaddr()
 	@trace(normal(0, 1), :x)
 	@trace(normal(0, 1), :x)
 end;
 
-# 2 - Exercise (2)
+# Part 2 - Exercise (2)
 @gen function sine(xs::Vector{float})
 	phase = @trace(uniform(-pi, pi), :phase)
 	period = @trace(gamma(5, 1), :period)
@@ -48,7 +48,7 @@ function sine_grid(renderer::Function, traces; ncols=6, nrows=3)
 	show()
 end;
 
-# 3 - Exercise
+# Part 3 - Exercise
 xs = collect(range(-5., stop=5.))
 ys_sine = [2.89, 2.22, -0.612, -0.522, -2.65, -0.133, 2.70, 2.77, 0.425, -2.11,
 	-2.76];
