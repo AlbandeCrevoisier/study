859720567461f728341e3545ce4b4416ef9d126f
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-04 11:05:37 +0200

    Add member?

--- little_schemer.scm
+++ little_schemer.scm
@@ -6,3 +6,9 @@
     ((null? l) #t)
     ((atom? (car l)) (lat? (cdr l)))
     (else #f)))
+
+(define (member? a lat)
+  (cond
+    ((null? lat) #f)
+    (else (or (eq? (car lat) a)
+              (member? a (cdr lat))))))
