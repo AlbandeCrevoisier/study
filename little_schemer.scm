33e1150f789c15c0247ff9e77d9445f532793cc1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-04 16:03:11 +0200

    Add makeset

--- little_schemer.scm
+++ little_schemer.scm
@@ -70,3 +70,12 @@
     ((null? lat) #t)
     ((member? (car lat) (cdr lat)) #f)
     (else (set? (cdr lat)))))
+
+(define (makeset lat)
+  (cond
+    ((null? lat) '())
+    ((member? (car lat) (cdr lat))
+     (makeset (cdr lat)))
+    (else (cons (car lat) (makeset (cdr lat))))))
+
+(display (makeset '(1 2 2 3)))
