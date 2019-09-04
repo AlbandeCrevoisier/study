570ac203e0913d5617ac90d64ea974612899497b
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-04 11:15:28 +0200

    Add member*

--- little_schemer.scm
+++ little_schemer.scm
@@ -18,3 +18,12 @@
     ((null? lat) '())
     ((eq? (car lat) a) (cdr lat))
     (else (cons (car lat) (rember a (cdr lat))))))
+
+(define (member* a l)
+  (cond
+    ((null? l) #f)
+    ((atom? (car l))
+     (or (eq? (car l) a)
+         (member* a (cdr l))))
+    (else (or (member* a (car l))
+              (member* a (cdr l))))))
