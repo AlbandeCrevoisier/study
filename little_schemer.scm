d172ddcc22a0bc67a9858ee39a8eec99ef2ea6d2
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-04 11:23:32 +0200

    Add eqan?

--- little_schemer.scm
+++ little_schemer.scm
@@ -27,3 +27,12 @@
          (member* a (cdr l))))
     (else (or (member* a (car l))
               (member* a (cdr l))))))
+
+(define (eqan? a1 a2)
+  (cond
+    ((and (number? a1) (number? a2))
+     (= a1 a2))
+    ((or (number? a1) (number? a2))
+     #f)
+    (else (eq? a1 a2))))
+
