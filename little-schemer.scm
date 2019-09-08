fe9ee685b82231ced87175dccd670bf30eff3af2
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-09 01:21:21 +0200

    Add missing parenthesis in evens-only*.

--- little-schemer.scm
+++ little-schemer.scm
@@ -218,6 +218,6 @@
      ((even? (car l))
       (cons (car l)
 	    (evens-only* (cdr l))))
-     (else evens-only* (cdr l))))
+     (else (evens-only* (cdr l)))))
    (else (cons (evens-only* (car l))
 	       (evens-only* (cdr l))))))
