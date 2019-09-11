05da9d369cf0b896573f9912efc9e4ff92f53bd1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-09-12 01:48:06 +0200

    Add lookup-in-entry-help.

--- little-schemer.scm
+++ little-schemer.scm
@@ -393,3 +393,14 @@
    (first entry)
    (second entry)
    entry-f))
+
+(define (lookup-in-entry-help name names values entry-f)
+  (cond
+   ((null? names) (entry-f name))
+   ((eq? (car names) name)
+    (car values))
+   (else (lookup-in-entry-help
+          name
+          (cdr names)
+          (cdr values)
+          entry-f))))
