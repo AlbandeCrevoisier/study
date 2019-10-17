50879c1723a3f33dc5a123c7b11c0e54a85c77ea
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-17 17:48:15 +0200

    Add scene & visualise it.

--- black_box_modeling.jl
+++ black_box_modeling.jl
@@ -2,3 +2,24 @@ using Gen, GenViz
 
 viz_server = VizServer(8090)
 sleep(1)
+
+include("qs/inverse-planning/geometric_primitives.jl")
+include("qs/inverse-planning/scene.jl")
+
+scene = Scene(0, 1, 0, 1)
+add_obstacle!(scene, make_square(Point(0.30, 0.20), 0.1))
+add_obstacle!(scene, make_square(Point(0.83, 0.80), 0.1))
+add_obstacle!(scene, make_square(Point(0.80, 0.40), 0.1))
+horizontal = false
+vertical = true
+wall_thickness = 0.02
+add_obstacle!(scene, make_line(horizontal, Point(0.20, 0.40), 0.40, wall_thickness))
+add_obstacle!(scene, make_line(vertical, Point(0.60, 0.40), 0.40, wall_thickness))
+add_obstacle!(scene, make_line(horizontal, Point(0.60 - 0.15, 0.80), 0.15 + wall_thickness, wall_thickness))
+add_obstacle!(scene, make_line(horizontal, Point(0.20, 0.80), 0.15, wall_thickness))
+add_obstacle!(scene, make_line(vertical, Point(0.20, 0.40), 0.40, wall_thickness))
+
+info = Dict("scene" => scene)
+viz = Viz(viz_server, joinpath(@__DIR__, "qs/inverse-planning/overlay-viz/dist"), info)
+openInBrowser(viz)
+sleep(999999)
