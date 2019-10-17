8940dc5ad0ce62cbb033bd862add9c5e237aa77a
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-17 18:14:54 +0200

    Compute & visualise path.

--- black_box_modeling.jl
+++ black_box_modeling.jl
@@ -5,6 +5,7 @@ sleep(1)
 
 include("qs/inverse-planning/geometric_primitives.jl")
 include("qs/inverse-planning/scene.jl")
+include("qs/inverse-planning/planning.jl")
 
 scene = Scene(0, 1, 0, 1)
 add_obstacle!(scene, make_square(Point(0.30, 0.20), 0.1))
@@ -19,7 +20,12 @@ add_obstacle!(scene, make_line(horizontal, Point(0.60 - 0.15, 0.80), 0.15 + wall
 add_obstacle!(scene, make_line(horizontal, Point(0.20, 0.80), 0.15, wall_thickness))
 add_obstacle!(scene, make_line(vertical, Point(0.20, 0.40), 0.40, wall_thickness))
 
-info = Dict("scene" => scene)
-viz = Viz(viz_server, joinpath(@__DIR__, "qs/inverse-planning/overlay-viz/dist"), info)
-openInBrowser(viz)
+start = Point(0.1, 0.1)
+dest = Point(0.5, 0.5)
+planner_params = PlannerParams(300, 3.0, 2000, 1.)
+path = plan_path(start, dest, scene, planner_params)
+
+info = Dict("start"=> start, "dest" => dest, "scene" => scene, "path_edges" => get_edges(path))
+viz_pa = Viz(viz_server, joinpath(@__DIR__, "qs/inverse-planning/overlay-viz/dist"), info)
+openInBrowser(viz_pa)
 sleep(999999)
