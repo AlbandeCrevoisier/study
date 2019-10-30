d0671c32363e823ce3779165d1629a81e3515a6e
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-30 16:07:20 +0100

    Ex 1.3

--- sicp/sicp.scm
+++ sicp/sicp.scm
@@ -31,3 +31,15 @@
    (* 3
       (- 6 2)
       (- 2 7)))
+
+
+;; Exercise 1.3
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
+
+(define (f a b c)
+  (cond ((and (< a b) (< a c))
+	 (sum-of-squares b c))
+	((and (< b a) (< b c))
+	 (sum-of-squares a c))
+	(else (sum-of-squares a b))))
