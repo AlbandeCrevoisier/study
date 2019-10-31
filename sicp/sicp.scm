0f2f77126c00fb524bcffd21a5bb8cf02bc34c0a
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-31 17:52:11 +0100

    Ex 1.9

--- sicp/sicp.scm
+++ sicp/sicp.scm
@@ -125,3 +125,26 @@
 
 (define (cube-root x)
   (cube-root-iter 1.0 x))
+
+
+;; Exercise 1.9
+;;
+;; (+ 4 5)
+;; (inc (+ 3 5))
+;; (inc (inc (+ 2 5)))
+;; (inc (inc (inc (+ 1 5))))
+;; (inc (inc (inc (inc (+ 0 5)))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
+;; This is clearly a linear recursive process.
+;;
+;; (+ 4 5)
+;; (+ 3 6)
+;; (+ 2 7)
+;; (+ 1 8)
+;; (+ 0 9)
+;; 9
+;; This is clearly an iterative process.
