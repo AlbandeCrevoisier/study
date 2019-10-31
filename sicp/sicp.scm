439e32b24afe57c77b1b30267680d9373d61e16d
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-31 16:25:55 +0100

    Ex 1.8

--- sicp/sicp.scm
+++ sicp/sicp.scm
@@ -107,3 +107,21 @@
 (sqrt 999999999999999)
 
 ;; This does not terminate with a threshold of 0.0000000001 .
+
+
+;; Exercise 1.8
+(define (cube x)
+  (* x x x))
+
+(define (improve-cube guess x)
+  (/ (+ (/ x (square guess))
+	(* 2 guess))
+     3))
+
+(define (cube-root-iter guess x)
+  (if (< (abs (- x (cube guess))) 0.001)
+      guess
+      (cube-root-iter (improve-cube guess x) x)))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
