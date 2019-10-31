95b169ea6bcf3c782c1c10267accdf700ba6be26
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-31 11:45:51 +0100

    Attempt an answer at Exercise 1.6.

--- sicp/sicp.scm
+++ sicp/sicp.scm
@@ -56,3 +56,25 @@
 ;; Exercise 1.5
 ;; With applicative-order evaluation: infinite-loop
 ;; With normal-order evaluation: 0
+
+
+;; Exercise 1.6
+;; Because of applicative-order evaluation, all the parameters will be
+;; evaluated before calling new-if, including the `else' call to
+;; `sqrt-iter', thus creating an infinite loop.
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
