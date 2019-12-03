d7f637e59927490a71beac9c11fda9b66343f35f
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-12-03 01:21:37 +0100

    Ex 2.2

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -37,3 +37,27 @@
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (cons (/ n g) (/ d g))))
+
+
+;; Exercise 2.2
+(define (make-segment p q) (cons p q))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (midpoint-segment s)
+  (make-point (average (x-point (start-segment s))
+                       (x-point (end-segment s)))
+              (average (y-point (start-segment s))
+                       (y-point (end-segment s)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ", ")
+  (display (y-point p))
+  (display ")"))
