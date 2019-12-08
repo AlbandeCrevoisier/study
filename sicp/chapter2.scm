cc532b3585a7a53a576f050099629b9a616b1a0e
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-12-09 00:46:28 +0100

    Ex 2.6

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -125,3 +125,13 @@
   (if (= (remainder p 3) 0)
       (+ 1 (p-cdr (/ p 3)))
       0))
+
+
+;; Exercise 2.6
+(define zero (lambda (f) (lambda (x) x)))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+(define (add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
