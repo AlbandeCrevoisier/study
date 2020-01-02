5da71ddc3c5014233306074ae913bd49322dc2cf
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-03 00:02:32 +0100

    Ex 2.38

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -505,3 +505,15 @@
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
     (map (lambda (line) (matrix-*-vector cols line)) m)))
+
+
+;; Exercise 2.38
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+;; op has to be commutative for fold-left & fold-right to be
+;; equivalent.
