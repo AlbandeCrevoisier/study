e62e2dc25d07bb5af89aec67a1cc18bfc755b07d
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-02 23:55:27 +0100

    Ex 2.37

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -493,3 +493,15 @@
       '()
       (cons (accumulate op init (map car seqs))
             (accumulate-n op init (map cdr seqs)))))
+
+
+;; Exercise 2.37
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+(define (matrix-*-vector m v)
+  (map (lambda (line) (dot-product line v)) m))
+(define (transpose m)
+  (accumulate-n cons '() m))
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (line) (matrix-*-vector cols line)) m)))
