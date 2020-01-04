a9399952d2c2fe838c629010baa5544dd61d1959
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-04 19:34:36 +0100

    Ex 2.42

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -508,7 +508,7 @@
 
 
 ;; Exercise 2.38
-(define (fold-right accumulate))
+(define fold-right accumulate)
 (define (fold-left op initial sequence)
   (define (iter result rest)
     (if (null? rest)
@@ -559,3 +559,34 @@
              (map (lambda (pair) (cons i pair))
                   (unique-pairs (- i 1))))
            (enumerate-interval 1 n)))
+
+
+;; Exercise 2.42
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+          (lambda (positions) (safe? k positions))
+          (flatmap
+            (lambda (rest-of-queens)
+              (map (lambda(new-row)
+                     (adjoin-position new-row k rest-of-queens))
+                   (enumerate-interval 1 board-size)))
+            (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (cons new-row k) rest-of-queens))
+(define (safe? k positions)
+  (if (<= k 1)
+      #t
+      (let ((row (caar positions)))
+        (null? (filter (lambda (pos)
+                         (let ((i (car pos))
+                               (j (cdr pos)))
+                           (or (= row i)
+                               (= row (+ i (- k j)))
+                               (= row (- i (- k j))))))
+                       (cdr positions))))))
