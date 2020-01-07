c7381ed8091a6f8c9e1f0ee925ea0165f166c6a0
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-07 01:25:48 +0100

    Ex 2.49 a.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -665,3 +665,11 @@
            (end-segment segment))))
       segment-list)))
 
+(define (outline frame)
+  ((segments->painter
+     (list
+       (make-segment (make-vect 0 0) (make-vect 0 1))
+       (make-segment (make-vect 0 1) (make-vect 1 1))
+       (make-segment (make-vect 1 1) (make-vect 1 0))
+       (make-segment (make-vect 1 0) (make-vect 0 0))))
+   frame))
