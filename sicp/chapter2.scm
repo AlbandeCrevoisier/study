2e3b7f190fbadedc248f6caa03587e09f2b2ad5f
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-07 01:30:48 +0100

    Ex 2.49 b.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -673,3 +673,11 @@
        (make-segment (make-vect 1 1) (make-vect 1 0))
        (make-segment (make-vect 1 0) (make-vect 0 0))))
    frame))
+(define (X frame)
+  ((segments->painter
+     (list
+       (make-segment (make-vect 0 0) (make-vect 0 1))
+       (make-segment (make-vect 0 1) (make-vect 1 1))
+       (make-segment (make-vect 1 1) (make-vect 1 0))
+       (make-segment (make-vect 1 0) (make-vect 0 0))))
+   frame))
