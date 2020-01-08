e2702b9ff4116ae201c4fb84e6de765dd6030964
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-09 00:49:48 +0100

    Ex 2.51

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -736,3 +736,19 @@
                      (make-vect 0 1)
                      (make-vect 0 0)
                      (make-vect 1 1)))
+
+
+;; Exercise 2.51
+(define (below painter1 painter2)
+  (rotate90 (beside (rotate270 painter1) (rotate270 painter2))))
+(define (below-long-version painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom (transform-painter (make-vect 0 0)
+                                           (make-vect 1 0)
+                                           split-point))
+          (paint-top (transform-painter split-point
+                                        (make-vect 1 0.5)
+                                        (make-vect 0 1))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
