846766b541f447636cab2b20dcc1a574f72fccb1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-09 00:49:57 +0100

    Ex 2.52

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -752,3 +752,18 @@
       (lambda (frame)
         (paint-bottom frame)
         (paint-top frame)))))
+
+
+;; Exercise 2.52
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((top-left (up-split painter (- n 1)))
+            (bottom-right (right-split painter (- n 1)))
+            (corner (corner-split painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner)))))
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four identity flip-hiroz
+                                  flip-vert rotate180)))
+  (combine4 (corner-split painter n))))
