f0f62ca7966675666f807fa24a06cc83ea992e50
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-12 01:45:40 +0100

    Add a basic differentiating program.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -800,3 +800,42 @@
 ;; Exercise 2.55
 ;; 'foo is extended in: (quote foo), hence ''foo is extended in:
 ;; (quote (quote foo)), which yields the list (quote foo).
+
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))))
+        (else
+          (error "Unknown expression type: DERIV" exp))))
+
+(define variable? symbol?)
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (sum? e)
+  (and (pair? e) (eq? (car e) '+)))
+(define addend cadr)
+(define augend caddr)
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* a1 a2))
+        (else (list '* m1 m2))))
+(define (product? e)
+  (and (pair? e) (eq? (car e) '*)))
+(define multiplier cadr)
+(define multiplicand caddr)
+(define (=number? exp num) (and (number? exp) (= exp num)))
