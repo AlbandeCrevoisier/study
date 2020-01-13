14e8209760abe078b3a8d63f5fa1427c8ef59dcf
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-14 00:25:45 +0100

    Ex 2.62

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -989,3 +989,18 @@
         (cond ((= x x1) set)
               ((> x x1) (cons x1 (adjoin-set x (cdr set))))
               (else (cons x set))))))
+
+
+;; Exercise 2.62
+;; union-set in Theta(n):
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((> x1 x2)
+                   (cons x2 (intersection-set set1 (cdr set2)))))))))
