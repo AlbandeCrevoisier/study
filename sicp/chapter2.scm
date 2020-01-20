49826b241170b8f3b2678ad5ae6d4f92e5fb87d6
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-21 00:10:08 +0100

    Ex 2.64 a.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1058,3 +1058,42 @@
 ;; b.
 ;; Both procedures have equivalent complexity growth,
 ;; in Theta(n).
+
+
+;; Exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result
+                (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result
+                    (partial-tree
+                      (cdr non-left-elts)
+                      right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts
+                      (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+;; a.
+;; partial-tree splits the n elements in three pools: the left part, on
+;; which it recursively runs, leaving no remaining element & generating
+;; a tree, the root which it extracts, & the right part on which it runs
+;; & returns a tree but no remaining element. It then makes a tree with
+;; the root as the root, the result of the left run as the left sub-
+;; tree & the result of the right run as the right-subtree. It then
+;; makes a pair with this tree and the remaining elements.
+;; (1 3 5 7 9 11) => 5
+;;                  / \
+;;                 1   9
+;;                 |  / \
+                   3 7   11
