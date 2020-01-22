700f6af6e8da97446b368e19f0a06c384afd5f37
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-01-23 00:41:32 +0100

    Ex 2.67 & subsequently correct code from the book.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1178,16 +1178,37 @@
           (if (leaf? next-branch)
               (cons (symbol-leaf next-branch)
                     (decode-1 (cdr bits) tree))
-              (decode-1 (cdr bits) tree)))))
+              (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
 
 (define (choose-branch bit branch)
   (cond ((= bit 0) (left-branch branch))
         ((= bit 1) (right-branch branch))
-        (else (error "bad bit: CHOOSE_BRANCH" bit))))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
 
 (define (adjoin-set x set)
   (cond ((null? set) (list x))
         ((< (weight x) (weight (car set))) (cons x set))
         (else (cons (car set)
                     (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)	; symbol
+                               (cadr pair))	; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+
+;; Exercise 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                    (make-leaf 'B 2)
+                    (make-code-tree
+                      (make-leaf 'D 1)
+                      (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+;; (decode sample-message sample-tree)
+;; => (a d a b b c a)
