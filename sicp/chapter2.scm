d9df39a35a556e78b92293c09a18584e1ca6a45c
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-04-17 11:55:19 +0200

    Ex 2.81 c.

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1430,3 +1430,28 @@ i;; Structure & Interpretation of Computer Programs
 ;; b. He is parially correct: if there is no operation found but the
 ;;    types are the same, then apply-generic will try to coerce the
 ;;    arguments instead of failing.
+;; c.
+(define (apply-generic op . args)
+       (let ((type-tags (map type-tag args)))
+         (let ((proc (get op type-tags)))
+           (if proc
+               (apply proc (map contents args))
+               (if (= (length args) 2)
+                   (let ((type1 (car type-tags))
+                         (type2 (cadr type-tags))
+                         (a1 (car args))
+                         (a2 (cadr args)))
+		     (if (eq? type1 type2)
+			 (error "No method for this type"
+				type1)
+			 (let ((t1->t2 (get-coercion type1 type2))
+			       (t2->t1 (get-coercion type2 type1)))
+			   (cond (t1->t2
+				  (apply-generic op (t1->t2 a1) a2))
+				 (t2->t1
+				  (apply-generic op a1 (t2->t1 a2)))
+				 (else
+				  (error "No method for these types"
+					 (list op type-tags))))))
+                   (error "No method for these types"
+                          (list op type-tags)))))))
