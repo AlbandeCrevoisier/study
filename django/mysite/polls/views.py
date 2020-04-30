dfd09587246030212a6c288bd4f4203345aa6015
Alban de Crevoisier <albandecrevoisier@gmail.com>
2020-04-30 15:17:59 +0200

    Type hints in polls views.

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def detail(request, question_id):
+def detail(request: HttpRequest, question_id: int):
     return HttpResponse("You're looking at question: %s." % question_id)
 
 
-def results(request, question_id):
+def results(request: HttpRequest, question_id: int):
     return HttpResponse("You're looking at the results of question: %s."
                         % question_id)
 
 
-def vote(request, question_id):
+def vote(request: HttpRequest, question_id: int):
     return HttpResponse("You're voting on question: %s" % question_id)
