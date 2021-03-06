a6a713ad9ef2777061e2c0482908d7a3505cf62f
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-03-06 21:40:27 +0100

    Add notebook for the 10-armed Testbed.

---
+++ rl-mooc/10-armed_testbed.jl
@@ -0,0 +1,16 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d0469884-7eba-11eb-1d19-eb3680e350a3
+md"
+# 10-armed Testbed
+
+10-armed bandit testbed to compare ϵ-greedy methods for various values of ϵ.
+
+"
+
+# ╔═╡ Cell order:
+# ╟─d0469884-7eba-11eb-1d19-eb3680e350a3
