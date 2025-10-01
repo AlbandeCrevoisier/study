5075227f4ed515cc0b07ee943261433e581e41f5
∇ <albandecrevoisier@gmail.com>
2025-10-01 19:09:22 +0200

    rust: add branches

---
+++ rust_book_experimental/branches/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+	let x = 42;
+	if x < 1337 {
+		println!("pleb");
+	} else if x == 1337 {
+		println!("elite!");
+	} else {
+		println!("showoff.");
+	}
+	let y = if true { 5 } else { 6 };
+	println!("{y}");
+}
